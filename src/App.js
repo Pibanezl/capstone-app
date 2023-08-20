455c1af16c400287132e757d4b9b161f9f71bef3
pibanezl <ped.ibanez@duocuc.cl>
2023-08-20 13:12:14 -0400

    cambios prueba

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import logo2 from './nolli.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
