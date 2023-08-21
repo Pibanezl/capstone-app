a42c1783cabb1ca4efa41db3ad862c8a1f356695
pibanezl <ped.ibanez@duocuc.cl>
2023-08-21 16:30:53 -0400

    cambiocolor boton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
+import React from 'react'
+import logo from './logo.svg';
 import logo2 from './no.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo2} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>acsmdh</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          aprenda React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  
+  cambioColor = () => {
+    alert('asdadasdasdasdasdasd');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo2} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>acsmdh</p>
+          <button onClick={cambioColor}>wea</button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            aprenda React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
