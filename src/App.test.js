5dba33c977dc55b59bda6650b2277b16e0f1ea9d
Jalcayaga <julioignaciorodriguez97@gmail.com>
2023-08-23 02:43:48 -0400

    pruebas automatizadas

--- src/App.test.js
+++ src/App.test.js
@@ -1,41 +1,21 @@
-/*
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
 import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import App from './App';
 
-test('debería cambiar el color del botón al hacer clic', () => {
-  // Arrange: Renderiza el componente
-  const { getByTestId } = render(<App />);
+test('clicking "Mostrar Usuarios" button toggles user section', () => {
+  const { getByTestId, queryByText } = render(<App />);
+  const button = getByTestId('mostrarUsuarios');
 
-  // Act: Simula un clic en el botón
-  const colorButton = getByTestId('cambioColor');
-  fireEvent.click(colorButton);
+  fireEvent.click(button);
 
-  // Assert: Verifica que el color de fondo haya cambiado a 'red'
-  expect(colorButton).toHaveStyle('background-color: red');
-
-  // Act: Simula otro clic en el botón
-  fireEvent.click(colorButton);
-
-  // Assert: Verifica que el color de fondo haya cambiado nuevamente a 'blue'
-  expect(colorButton).toHaveStyle('background-color: blue');
+  expect(queryByText('Administración de Usuarios')).toBeInTheDocument();
 });
 
-test('debería cambiar la fuente del botón al hacer clic', () => {
-  // Arrange: Renderiza el componente
-  const { getByTestId } = render(<App />);
+test('clicking "Mostrar Ciudades" button toggles city section', () => {
+  const { getByTestId, queryByText } = render(<App />);
+  const button = getByTestId('mostrarCiudades');
 
-  // Act: Simula un clic en el botón
-  const fontButton = getByTestId('cambioFuente');
-  fireEvent.click(fontButton);
+  fireEvent.click(button);
 
-  // Assert: Verifica que la fuente haya cambiado a 'arial'
-  expect(fontButton).toHaveStyle('font-family: arial;');
-
-  // Act: Simula otro clic en el botón
-  fireEvent.click(fontButton);
-
-  // Assert: Verifica que la fuente haya cambiado nuevamente a 'roboto'
-  expect(fontButton).toHaveStyle('font-family: roboto;');
+  expect(queryByText('Lista de Ciudades')).toBeInTheDocument();
 });
-*/
