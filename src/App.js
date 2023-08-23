d9b8d664c6f792bd2a37fcd51d850b5f01259df1
hypermegadev <ped.ibanez@duocuc.cl>
2023-08-23 01:40:17 -0400

    loading integration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import './App.css';
+import Loading from 'react-loading';
 
 import  useCollectionData  from './firebase_setup/useCollectionData';
-
+import UserCrud from './firebase_setup/crud';
 const App = () => {
   const collectionUsuarios = "usuarios"; // Nombre de la colección deseada
   const collectionCiudades = "ciudades"; // Nombre de la colección deseada
@@ -35,17 +36,14 @@ const App = () => {
             <button data-testid="mostrarCiudades" className="mostrarCiudades" id="mostrarCiudades" onClick={mostrarCiudades} >mostrar ciudades</button>
           </div>
         </section>
-        {showUsuarios && usuariosData && (
+        {showUsuarios && (
         <section className="seccionUsuarios">
-          <h2>Lista de Usuarios</h2>
-          <ul>
-            {usuariosData.map((usuario, index) => (
-              <li key={index}>
-                {usuario.data.nombre} {usuario.data.apellido}
-              </li>
-            ))}
-          </ul>
-        </section>
+        {usuariosData ? (
+          <UserCrud />
+        ) : (
+          <Loading type="spin" color="#000000" height={50} width={50} />
+        )}
+      </section>
       )}
       {showCiudades && ciudadesData &&(
         <section className="seccionCiudades">
