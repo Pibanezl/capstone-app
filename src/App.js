daac2bd4ade06ade72d4c33e43445f321edde287
hypermegadev <ped.ibanez@duocuc.cl>
2023-08-24 21:41:47 -0400

    color

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = () => {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Portafolio de Títuloooooo</h1>
+          <h1 style="color: red;">Portafolio de Título</h1>
         </header>
         <section className="seccionBotones">
           <div className="contenedorBotones">
