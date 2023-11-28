51d8bb9b1d2a35ada7358815957606f438454a76
pibanez <ped.ibanez@duocuc.cl>
2023-11-27 18:31:23 -0600

    css home

---
+++ src/components/sobre-nosotros.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react';
+import "../styles/sass/banner-principal.scss";
+import "../styles/sass/sobre-nosotros.scss";
+
+function SobreNosotros() {
+    return (
+        <>
+        <div id="about-us-text">CMT panels have been in the industry for over 30 years. Our aim is to bring your project vision to reality.
+
+With unrivaled experience in the sandwich panel and cool room industry, you can rest assured knowing
+
+that our team of accredited builders, installers and project managers will assist you with your project
+
+from start to finish no matter how big or small.
+
+CMT panels is leading the industry by staying abreast with technology using the latest in panel installing
+
+techniques, we have been trusted to play a large part in installing some of Australasia&#39;s biggest projects.
+
+Using only the best available materials on the market and a focus on quality, you can be at ease knowing
+
+that you are in good hands at CMT panels.</div>
+        <div id="page-name-bar-heading">
+            <p id="p-n-b-h">About Us</p>
+            <p id="p-n-b-h2">A LITTLE SOMETHING ABOUT US</p>
+            <p id="p-n-b-h3">OUR HISTORY AND ACHIEVEMENTS</p>
+        </div><section class="timeline">
+                <div class="wrapper">
+                    <div class="timeline__item timeline__item--0">
+                        <div class="timeline__item__station"></div>
+                        <div class="timeline__item__content">
+                            <h2 class="timeline__item__content__date">1987 Aug. 20</h2>
+                            <p class="timeline__item__content__description">Began learning in the panel industry by working with some of Australia biggest companies including pacesetter, portacom, Ausco and James Hardies.</p>
+                        </div>
+                    </div>
+                    <div class="timeline__item timeline__item--1">
+                        <div class="timeline__item__station"></div>
+                        <div class="timeline__item__content">
+                            <h2 class="timeline__item__content__date">1998 Sept. 5</h2>
+                            <p class="timeline__item__content__description">Started installing various cool rooms and freezers around the country.</p>
+                        </div>
+                    </div>
+                    <div class="timeline__item timeline__item--2">
+                        <div class="timeline__item__station"></div>
+                        <div class="timeline__item__content">
+                            <h2 class="timeline__item__content__date">1995 Mar. 5</h2>
+                            <p class="timeline__item__content__description">As well as contracting for ALL the major sandwich panel building companies, we began to take a keen interest in cool room construction.</p>
+                        </div>
+                    </div>
+                    <div class="timeline__item timeline__item--3">
+                        <div class="timeline__item__station"></div>
+                        <div class="timeline__item__content">
+                            <h2 class="timeline__item__content__date">2000 July 9</h2>
+                            <p class="timeline__item__content__description">On top of cool room, chassis and portable building fabrication , we begin a partnership with one of Australia's largest manufacturer of switch rooms, temperature controlled shelters and specific designed transportable rooms furthering our knowledge in this field. To date close to 9000 sandwich panel buildings have been manufactured by CMT panels with our partner.</p>
+                        </div>
+                    </div>
+                    <div class="timeline__item timeline__item--4">
+                        <div class="timeline__item__station"></div>
+                        <div class="timeline__item__content">
+                            <h2 class="timeline__item__content__date">2003 Nov. 12</h2>
+                            <p class="timeline__item__content__description">We become what we are now known as CMT Panels.</p>
+                        </div>
+                    </div>
+                    <div class="timeline__item timeline__item--5">
+                        <div class="timeline__item__station"></div>
+                        <div class="timeline__item__content">
+                            <h2 class="timeline__item__content__date">2009 Aug. 25</h2>
+                            <p class="timeline__item__content__description">We begin our part in the Gorgon project, Barrow Island WA. This was at the time one of the largest construction jobs hapenning in the world.</p>
+                        </div>
+                    </div>
+                    <div class="timeline__item timeline__item--6">
+                        <div class="timeline__item__station"></div>
+                        <div class="timeline__item__content">
+                            <h2 class="timeline__item__content__date">2011 Aug. 03</h2>
+                            <p class="timeline__item__content__description">We move premises to Thomaston in Victoria to help accommodate larger projects and expand.</p>
+                        </div>
+                    </div>
+                    <div class="timeline__item timeline__item--7">
+                        <div class="timeline__item__station"></div>
+                        <div class="timeline__item__content">
+                            <h2 class="timeline__item__content__date">2012 Jan. 24</h2>
+                            <p class="timeline__item__content__description">We begin our part on the Wheatstone project, Onslow WA, also one of the largest construction jobs in the world.</p>
+                        </div>
+                    </div>
+                </div>
+            </section></>
+    );
+}
+
+export default SobreNosotros;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import AdminView from "../components/AdminView";
 import UserView from "../components/UserView";
 import BannerPrincipal from '../components/banner-principal';
+import SobreNosotros from '../components/sobre-nosotros';
 /*import StudentGenerator from "../components/generator-students"
 import StudentGeneratorIncident from "../components/generator-incident-student"*/
 import ClonarColeccion from "../components/cambiar-coleccion"
@@ -12,6 +13,7 @@ function Home({ user }) {
       {user === null ? (
       <div className="home-comun">
         <BannerPrincipal/>
+        <SobreNosotros/>
         </div>) :
         (<div>
           Home
